331b1c92c2d94d0de21ddde68c6b897919097127
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2024-11-27 15:57:50 +0100

    creer le menu

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>// Requise pour les entrées/sorties.
+#include <string.h>//
+int main(){
+
+
+		printf("\n  Menu \n");
+		printf("1-ajouter \n");
+		printf("2-afficher \n");
+		printf("3-modifier \n ");	
+		printf("4-Supprimer \n");
+        printf("5-filtrer \n");
+        printf("6-quitter \n");
+		
+}
+
+
